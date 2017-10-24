e8278d5bdcbd917c487b47bd4bd5da77a9c5e1e3
igniteui-deploy <igniteui@infragistics.com>
2017-10-24 12:35:42 +0000

    Release 17.2.0-rc.6

--- js/modules/i18n/infragistics.ui.tilemanager-de.js
+++ js/modules/i18n/infragistics.ui.tilemanager-de.js
@@ -1,5 +1,5 @@
 /*!@license
-* Infragistics.Web.ClientUI Tile Manager localization resources 17.2.0-rc.5
+* Infragistics.Web.ClientUI Tile Manager localization resources 17.2.0-rc.6
 *
 * Copyright (c) 2011-2017 Infragistics Inc.
 *
